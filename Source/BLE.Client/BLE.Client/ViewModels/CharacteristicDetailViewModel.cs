4e4a56fd36f3d221bd851f769bc6b6de5dc69f34
Mark J Radacz <radacz.mcp@gmail.com>
2020-06-26 05:57:45 -0400

    write/notify on same page

--- Source/BLE.Client/BLE.Client/ViewModels/CharacteristicDetailViewModel.cs
+++ Source/BLE.Client/BLE.Client/ViewModels/CharacteristicDetailViewModel.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Acr.UserDialogs;
 using MvvmCross;
 using MvvmCross.Commands;
@@ -11,6 +13,7 @@ using Plugin.BLE.Abstractions;
 using Plugin.BLE.Abstractions.Contracts;
 using Plugin.BLE.Abstractions.EventArgs;
 using Plugin.BLE.Abstractions.Extensions;
+using Xamarin.Forms;
 
 namespace BLE.Client.ViewModels
 {
@@ -18,13 +21,35 @@ namespace BLE.Client.ViewModels
     {
         private readonly IUserDialogs _userDialogs;
         private bool _updatesStarted;
+        private IService _service;
+
         public ICharacteristic Characteristic { get; private set; }
+        public IReadOnlyList<IDescriptor> Descriptors { get; private set; }
+        public ICharacteristic WriteCharacteristic { get; private set; }
+
+        public static Guid SKY_HUBSERVICE_GUID = new Guid("3e520001-1368-b682-4440-d7dd234c45bc");
+        public static Guid SKY_HUB_WRITE_CHAR_GUID = new Guid("3e520002-1368-b682-4440-d7dd234c45bc");
+        public static Guid SKY_HUB_NOTIFY_CHAR_GUID = new Guid("3e520003-1368-b682-4440-d7dd234c45bc");
 
         public string CharacteristicValue => Characteristic?.Value.ToHexString().Replace("-", " ");
+        public string WriteCharacteristicValue => WriteCharacteristic?.Value.ToHexString().Replace("-", " ");
 
         public ObservableCollection<string> Messages { get; } = new ObservableCollection<string>();
 
         public string UpdateButtonText => _updatesStarted ? "Stop updates" : "Start updates";
+
+        public string WritePermissions
+        {
+            get
+            {
+                if (WriteCharacteristic == null)
+                    return string.Empty;
+
+                return (WriteCharacteristic.CanRead ? "Read " : "") +
+                       (WriteCharacteristic.CanWrite ? "Write " : "") +
+                       (WriteCharacteristic.CanUpdate ? "Update" : "");
+            }
+        }
 
         public string Permissions
         {
@@ -39,7 +64,7 @@ namespace BLE.Client.ViewModels
             }
         }
 
-        public CharacteristicDetailViewModel(IAdapter adapter, IUserDialogs userDialogs) : base(adapter)
+        public CharacteristicDetailViewModel(IAdapter adapter, IUserDialogs userDialogs, IMvxNavigationService navigation) : base(adapter)
         {
             _userDialogs = userDialogs;
         }
@@ -48,7 +73,14 @@ namespace BLE.Client.ViewModels
         {
             base.Prepare(parameters);
 
-            Characteristic = await GetCharacteristicFromBundleAsync(parameters);
+            //Characteristic = await GetCharacteristicFromBundleAsync(parameters);
+
+            _service = await GetServiceFromBundleAsync(parameters);
+
+            WriteCharacteristic = await _service.GetCharacteristicAsync(SKY_HUB_WRITE_CHAR_GUID);
+            Characteristic = await _service.GetCharacteristicAsync(SKY_HUB_NOTIFY_CHAR_GUID);
+            Descriptors = await Characteristic.GetDescriptorsAsync();
+
         }
 
         public override void ViewAppeared()
@@ -60,8 +92,6 @@ namespace BLE.Client.ViewModels
                 return;
             }
 
-            var navigation = Mvx.IoCProvider.Resolve<IMvxNavigationService>();
-            navigation.Close(this);
         }
         public override void ViewDisappeared()
         {
@@ -74,7 +104,7 @@ namespace BLE.Client.ViewModels
             
         }
 
-        public MvxCommand ReadCommand => new MvxCommand(ReadValueAsync);
+        public Command ReadCommand => new Command(ReadValueAsync);
 
         private async void ReadValueAsync()
         {
@@ -110,24 +140,31 @@ namespace BLE.Client.ViewModels
 
         private async void WriteValueAsync()
         {
+            var hexStr = "00 23 00 02 10 00 00 01 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 78 2D";
             try
             {
                 var result =
                     await
                         _userDialogs.PromptAsync("Input a value (as hex whitespace separated)", "Write value",
-                            placeholder: CharacteristicValue);
+                            placeholder: hexStr);
 
                 if (!result.Ok)
                     return;
 
-                var data = GetBytes(result.Text);
+                //var data = GetBytes(result.Text);
+                var data = GetBytes(hexStr);
 
                 _userDialogs.ShowLoading("Write characteristic value");
-                await Characteristic.WriteAsync(data);
+
+                Device.BeginInvokeOnMainThread(async () => 
+                {
+                    await WriteCharacteristic.WriteAsync(data);
+                });
+
                 _userDialogs.HideLoading();
 
-                await RaisePropertyChanged(() => CharacteristicValue);
-                Messages.Insert(0, $"Wrote value {CharacteristicValue}");
+                await RaisePropertyChanged(() => WriteCharacteristicValue);
+                Messages.Insert(0, $"Wrote value {WriteCharacteristicValue}");
             }
             catch (Exception ex)
             {
@@ -146,15 +183,21 @@ namespace BLE.Client.ViewModels
         {
             if (_updatesStarted)
             {
-                StopUpdates();
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await StopUpdates();
+                });
             }
             else
             {
-                StartUpdates();
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await StartUpdates();
+                });
             }
         }));
 
-        private async void StartUpdates()
+        private async Task StartUpdates()
         {
             try
             {
@@ -167,6 +210,8 @@ namespace BLE.Client.ViewModels
 
                 Messages.Insert(0, $"Start updates");
 
+                Descriptors = await Characteristic.GetDescriptorsAsync();
+
                 await RaisePropertyChanged(() => UpdateButtonText);
 
             }
@@ -176,7 +221,7 @@ namespace BLE.Client.ViewModels
             }
         }
 
-        private async void StopUpdates()
+        private async Task StopUpdates()
         {
             try
             {
